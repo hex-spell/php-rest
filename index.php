e5401a720889a8f1730c8842f53c9ac1c28e936d
Vagus-art <agustin.favo@gmail.com>
2020-04-12 19:17:58 -0300

    restruct everything to handle different request methods

--- index.php
+++ index.php
@@ -1,32 +1,74 @@
 <?php
-//ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 include('dbconfig-mysql.php');
-$connection = OpenCon();
 
-$search = $_GET['search'];
-$id = $_GET['id'];
+$DBconnection = OpenCon();
 
-function sanitize($string){
-    return filter_var(trim($string),FILTER_SANITIZE_STRING);
+function sanitize($string)
+{
+    return filter_var(trim($string), FILTER_SANITIZE_STRING);
 }
 
-if ($search&&!$id){
-    $query = "SELECT * FROM contacts WHERE LOWER(name) LIKE " . "'%" . sanitize(strtolower($search)) . "%'" . " LIMIT 10";
-}
-else if($id&&!$search){
-    $query = "SELECT * FROM contacts WHERE id = " . sanitize($id);
-}
-else {
-    $query = "SELECT * FROM contacts LIMIT 10";
+function serverError()
+{
+    echo "this request won't work";
 }
 
-$result = mysqli_query($connection,$query) or die('La consulta fallo: ' . pg_last_error());
+function getContacts($DBconnection)
+{
+    try {
 
-$response = array();
+        $search = $_GET['search'];
+        $id = $_GET['id'];
 
-while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-    $response[] = $row;
+        if ($search && !$id) {
+            $query = "SELECT * FROM contacts WHERE LOWER(name) LIKE " . "'%" . sanitize(strtolower($search)) . "%'" . " LIMIT 10";
+        } else if ($id && !$search) {
+            $query = "SELECT * FROM contacts WHERE id = " . sanitize($id);
+        } else {
+            $query = "SELECT * FROM contacts LIMIT 10";
+        }
+
+        $result = mysqli_query($DBconnection, $query) or die('La consulta fallo');
+
+        $response = array();
+
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $response[] = $row;
+        }
+
+        echo json_encode($response);
+
+    } catch (Exception $error) {
+        serverError();
+    }
 }
 
-echo json_encode($response);
-?>
+function postContacts(){
+    echo "posted!";
+}
+
+function updateContacts(){
+    echo "updated!";
+}
+
+function deleteContact(){
+    echo "deleted!";
+}
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        getContacts($DBconnection);
+        break;
+    case 'POST':
+        postContacts();
+    break;
+    case 'PUT':
+        updateContacts();
+    break;
+    case 'DELETE':
+        deleteContact();
+    break;
+    default:
+        serverError();
+}
